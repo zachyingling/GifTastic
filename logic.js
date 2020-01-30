1debee38faffc31d22beb6dde20f1d2cbaa1b6d9
Zachary <zpyingling9559@gmail.com>
2020-01-29 21:59:39 -0500

    finishing required. making the rating and gifs append in the gifs div. aswell as making them be able to be paused on click.

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-// Add function to update buttons, add ajax call to pull from giphy, add event listeners (one for adding an animal and one for clicking on one of the buttons on the top row)
+// add ajax call to pull from giphy, add event listeners (one for clicking on one of the buttons on the top row)
 
 $(document).ready(function() {
   let animals = [
@@ -22,13 +22,55 @@ $(document).ready(function() {
     }
   }
 
-  $("#submit-button").click(function() {
-    if ($("#input-box").val() === undefined) {
+  $("#submit-button").on("click", function() {
+    if ($("#input-box").val() === "") {
       return;
     } else {
       let inputValue = $("#input-box").val();
       animals.push(inputValue);
       updateButtons();
+    }
+  });
+
+  // Code to get the data in the button value to go to the giphy api and display the different gifs using the button's value
+  $(document).on("click", ".button", function() {
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?api_key=bMiBd76ARCGzlX5jLEc5f9DceyPBVcTK&q=";
+    let searchParameter = $(this).val();
+    let finalURL = queryURL + searchParameter;
+
+    $.ajax({
+      url: finalURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      $(".gifs").empty();
+      for (let i = 0; i < 10; i++) {
+        let newDiv = $("<div class='gif-box'>");
+        newDiv.append("<h2>Rating: " + response.data[i].rating + "</h2>");
+        newDiv.append(
+          "<img src='" +
+            response.data[i].images.downsized.url +
+            "' alt='" +
+            searchParameter +
+            " gif' class='gif-image' data-state='animate' data-animate='" +
+            response.data[i].images.downsized.url +
+            "' data-still='" +
+            response.data[i].images.downsized_still.url +
+            "'/>"
+        );
+        $(".gifs").append(newDiv);
+      }
+    });
+  });
+
+  $(document).on("click", ".gif-image", function() {
+    if ($(this).attr("data-state") === "animate") {
+      $(this).attr("data-state", "still");
+      $(this).attr("src", $(this).attr("data-still"));
+    } else {
+      $(this).attr("data-state", "animate");
+      $(this).attr("src", $(this).attr("data-animate"));
     }
   });
 });
